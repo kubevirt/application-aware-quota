b1e1bbce4c7b66e65ce5b8fd99e13da8a04f0bbc
Vladik Romanovsky <vromanso@redhat.com>
2023-09-08 13:52:30 -0400

    generate kubevirt clientset aaq separatelly
    Signed-off-by: Vladik Romanovsky <vromanso@redhat.com>

--- hack/update-codegen.sh
+++ hack/update-codegen.sh
@@ -28,16 +28,22 @@ CODEGEN_PKG=${CODEGEN_PKG:-$(
 find "${SCRIPT_ROOT}/pkg/" -name "*generated*.go" -exec rm {} -f \;
 find "${SCRIPT_ROOT}/staging/src/kubevirt.io/applications-aware-quota-api/" -name "*generated*.go" -exec rm {} -f \;
 rm -rf "${SCRIPT_ROOT}/pkg/generated"
-
 # generate the code with:
 # --output-base    because this script should also be able to run inside the vendor dir of
 #                  k8s.io/kubernetes. The output-base is needed for the generators to output into the vendor dir
 #                  instead of the $GOPATH directly. For normal projects this can be dropped.
 /bin/bash ${CODEGEN_PKG}/generate-groups.sh  "deepcopy,client,informer,lister" \
-  kubevirt.io/applications-aware-quota/pkg/generated \
+  kubevirt.io/applications-aware-quota/pkg/generated/aaq \
   kubevirt.io/applications-aware-quota/staging/src/kubevirt.io/applications-aware-quota-api/pkg/apis  \
     "core:v1alpha1 " \
     --go-header-file ${SCRIPT_ROOT}/hack/custom-boilerplate.go.txt
+
+/bin/bash ${CODEGEN_PKG}/generate-groups.sh  "client" \
+  kubevirt.io/applications-aware-quota/pkg/generated/kubevirt \
+  kubevirt.io/api  \
+    "core:v1 " \
+    --go-header-file ${SCRIPT_ROOT}/hack/custom-boilerplate.go.txt
+
 echo "************* running controller-gen to generate schema yaml ********************"
 (
     mkdir -p "${SCRIPT_ROOT}/_out/manifests/schema"
