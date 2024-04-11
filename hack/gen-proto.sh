db128fab0f59dc3b885d7a1de2398b19586db90b
bmordeha <bmordeha@redhat.com>
2024-04-11 13:23:55 +0300

    Introduce library for easy side car implementation To implement the sidecar container just implement SidecarCalculator by implementing podUsageFunc.
    Signed-off-by: bmordeha <bmordeha@redhat.com>

--- hack/gen-proto.sh
+++ hack/gen-proto.sh
@@ -5,4 +5,5 @@ set -e
 source $(dirname "$0")/build/common.sh
 source $(dirname "$0")/build/config.sh
 
-protoc --go_out=plugins=grpc:. staging/evaluator-server-com/evaluate.proto
+protoc --go_out=plugins=grpc:. staging/libsidecar/evaluator-server-com/evaluate.proto
+cp staging/libsidecar/evaluator-server-com/evaluate.pb.go pkg/util/net/generated
