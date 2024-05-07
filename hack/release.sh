db4938238a7056afae4706cb7c94105736be2c42
bmordeha <bmordeha@redhat.com>
2024-05-07 09:11:40 +0300

    Add requirement for release tool for safe publishing
    The current process for creating a Z-stream release
     for AAQ requires us to force push a new tag.
    
    This approach is not safe, as it requires manual
    push to the AAQ repository and can cause issues.
    
    To avoid this, we should align with the
    kubevirt/kubevirt project and utilize
    the release tool.
    
    Signed-off-by: bmordeha <bmordeha@redhat.com>

---
+++ hack/release.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+
+source $(dirname "$0")/build/common.sh
+source $(dirname "$0")/build/config.sh
+
+set -e
+
+if [ -z "${AAQ_CRI}" ]; then
+    echo >&2 "no working container runtime found. Neither docker nor podman seems to work."
+    exit 1
+fi
+
+if [ -z ${GPG_PRIVATE_KEY_FILE} ]; then
+    echo "GPG_PRIVATE_KEY_FILE env var must be set"
+    exit 1
+elif [ -z ${GPG_PASSPHRASE_FILE} ]; then
+    echo "GPG_PASSPHRASE_FILE env var must be set"
+    exit 1
+elif [ -z ${GITHUB_API_TOKEN_FILE} ]; then
+    echo "GITHUB_API_TOKEN_FILE env var must be set"
+    exit 1
+fi
+
+GIT_USER=${GIT_USER:-$(git config user.name)}
+GIT_EMAIL=${GIT_EMAIL:-$(git config user.email)}
+
+echo "git user:  $GIT_USER"
+echo "git email: $GIT_EMAIL"
+
+${AAQ_CRI} pull quay.io/kubevirtci/release-tool:latest
+
+echo "${AAQ_CRI} run -it --rm \
+-v ${GPG_PRIVATE_KEY_FILE}:/home/releaser/gpg-private \
+-v ${GPG_PASSPHRASE_FILE}:/home/releaser/gpg-passphrase \
+-v ${GITHUB_API_TOKEN_FILE}:/home/releaser/github-api-token \
+-v /home/.gitconfig:/home/releaser/.gitconfig \
+quay.io/kubevirtci/release-tool:latest \
+--org=kubevirt \
+--repo=application-aware-quota \
+--git-email \"${GIT_EMAIL}\" \
+--git-user \"${GIT_USER}\"
+\"$@\""
+
+${AAQ_CRI} run -it --rm \
+    -v ${GPG_PRIVATE_KEY_FILE}:/home/releaser/gpg-private \
+    -v ${GPG_PASSPHRASE_FILE}:/home/releaser/gpg-passphrase \
+    -v ${GITHUB_API_TOKEN_FILE}:/home/releaser/github-api-token \
+    -v /home/.gitconfig:/home/.gitconfig \
+    quay.io/kubevirtci/release-tool:latest \
+    --org=kubevirt \
+    --repo=application-aware-quota \
+    --git-email "${GIT_EMAIL}" \
+    --git-user "${GIT_USER}" \
+    "$@"
