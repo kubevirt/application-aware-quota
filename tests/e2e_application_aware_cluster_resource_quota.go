01fc6c64c998cbe1b4eaf3dafd355cd7d7b9b850
bmordeha <bmordeha@redhat.com>
2024-05-16 12:14:42 +0300

    Add functional test to make sure acrqs label selector result schedulingGates on pods
    Signed-off-by: bmordeha <bmordeha@redhat.com>

--- tests/e2e_application_aware_cluster_resource_quota.go
+++ tests/e2e_application_aware_cluster_resource_quota.go
@@ -2,11 +2,13 @@ package tests
 
 import (
 	"context"
+	"fmt"
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
 	v1 "k8s.io/api/core/v1"
 	"k8s.io/apimachinery/pkg/api/resource"
 	v12 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"kubevirt.io/application-aware-quota/staging/src/kubevirt.io/application-aware-quota-api/pkg/apis/core/v1alpha1"
 	"kubevirt.io/application-aware-quota/tests/builders"
 	"kubevirt.io/application-aware-quota/tests/framework"
 	"kubevirt.io/application-aware-quota/tests/libaaq"
@@ -32,7 +34,6 @@ var _ = Describe("ApplicationAwareAppliedClusterResourceQuota", func() {
 		labelSelector = &v12.LabelSelector{
 			MatchLabels: map[string]string{"foo": "foo"},
 		}
-		// Function to add label to a namespace
 		err = utils.AddLabelToNamespace(f.K8sClient, "default", "foo", "foo")
 		Expect(err).ToNot(HaveOccurred())
 		err = utils.AddLabelToNamespace(f.K8sClient, f.Namespace.GetName(), "foo", "foo")
@@ -63,6 +64,11 @@ var _ = Describe("ApplicationAwareAppliedClusterResourceQuota", func() {
 
 			By("Creating a ApplicationAwareClusterResourceQuota")
 			_, err := utils.CreateApplicationAwareClusterResourceQuota(ctx, f.AaqClient, acrq)
+			Expect(err).ToNot(HaveOccurred())
+
+			err = utils.AddLabelToNamespace(f.K8sClient, "default", "foo", "foo")
+			Expect(err).ToNot(HaveOccurred())
+			err = utils.AddLabelToNamespace(f.K8sClient, f.Namespace.GetName(), "foo", "foo")
 			Expect(err).ToNot(HaveOccurred())
 
 			By("Waiting for the ApplicationAwareClusterResourceQuota to be propagated")
@@ -110,6 +116,7 @@ var _ = Describe("ApplicationAwareAppliedClusterResourceQuota", func() {
 		})
 
 		It("Removing a cluster quota should change the quota-namespace mapping and trigger the gate controller", func(ctx context.Context) {
+
 			acrq := builders.NewAcrqBuilder().
 				WithName("test-quota").
 				WithLabelSelector(labelSelector).
@@ -126,7 +133,9 @@ var _ = Describe("ApplicationAwareAppliedClusterResourceQuota", func() {
 			Expect(err).ToNot(HaveOccurred())
 			_, err = utils.CreateApplicationAwareClusterResourceQuota(ctx, f.AaqClient, acrq2)
 			Expect(err).ToNot(HaveOccurred())
-
+			By("Add label to include test namespace in the acrq")
+			err = utils.AddLabelToNamespace(f.K8sClient, f.Namespace.GetName(), "foo", "foo")
+			Expect(err).ToNot(HaveOccurred())
 			By("Waiting for the ApplicationAwareClusterResourceQuotas to be propagated")
 			expectedRresources := v1.ResourceList{}
 			expectedRresources[v1.ResourceRequestsMemory] = resource.MustParse("0")
@@ -152,4 +161,56 @@ var _ = Describe("ApplicationAwareAppliedClusterResourceQuota", func() {
 			utils.VerifyPodIsNotGated(f.K8sClient, f.Namespace.Name, pod.Name)
 		})
 	})
+
+	Context("Test gating with ACRQs", func() {
+		It("Gate should be added to a pod only if acrq is enforced on it and should no longer be added if relevant label is no longer in a namespace", func() {
+			labelSelector := &v12.LabelSelector{
+				MatchLabels: map[string]string{"foo": "foo"},
+			}
+			acrq := builders.NewAcrqBuilder().WithName("acrq").WithResource(v1.ResourcePods, resource.MustParse("0")).WithLabelSelector(labelSelector).Build()
+			_, err := f.AaqClient.AaqV1alpha1().ApplicationAwareClusterResourceQuotas().Create(context.Background(), acrq, v12.CreateOptions{})
+			Expect(err).ToNot(HaveOccurred())
+			pod := utils.NewTestPodForQuota("somepod", v1.ResourceList{}, v1.ResourceList{})
+			for i := 0; i < 3; i++ { //retry 3 times to make sure it is not race-full
+				err := utils.AddLabelToNamespace(f.K8sClient, f.Namespace.GetName(), "foo", "foo")
+				Expect(err).ToNot(HaveOccurred())
+				Eventually(func() error {
+					acrq, err := f.AaqClient.AaqV1alpha1().ApplicationAwareClusterResourceQuotas().Get(context.Background(), acrq.Name, v12.GetOptions{})
+					Expect(err).ToNot(HaveOccurred())
+					if !nsIncludedInAcrq(acrq, f.Namespace.Name) {
+						return fmt.Errorf("acrq should include test ns")
+					}
+					return nil
+				}, 1*time.Minute, 1*time.Second).ShouldNot(HaveOccurred())
+				_, err = f.K8sClient.CoreV1().Pods(f.Namespace.Name).Create(context.Background(), pod, v12.CreateOptions{})
+				Expect(err).ToNot(HaveOccurred())
+				utils.VerifyPodIsGated(f.K8sClient, f.Namespace.Name, pod.Name)
+				err = f.K8sClient.CoreV1().Pods(f.Namespace.Name).Delete(context.Background(), pod.Name, v12.DeleteOptions{})
+				Expect(err).ToNot(HaveOccurred())
+				err = utils.RemoveLabelFromNamespace(f.K8sClient, f.Namespace.GetName(), "foo")
+				Expect(err).ToNot(HaveOccurred())
+				Eventually(func() error {
+					acrq, err := f.AaqClient.AaqV1alpha1().ApplicationAwareClusterResourceQuotas().Get(context.Background(), acrq.Name, v12.GetOptions{})
+					Expect(err).ToNot(HaveOccurred())
+					if nsIncludedInAcrq(acrq, f.Namespace.Name) {
+						return fmt.Errorf("acrq should not include test ns")
+					}
+					return nil
+				}, 1*time.Minute, 1*time.Second).ShouldNot(HaveOccurred())
+				_, err = f.K8sClient.CoreV1().Pods(f.Namespace.Name).Create(context.Background(), pod, v12.CreateOptions{})
+				utils.VerifyPodIsNotGated(f.K8sClient, f.Namespace.Name, pod.Name)
+				err = f.K8sClient.CoreV1().Pods(f.Namespace.Name).Delete(context.Background(), pod.Name, v12.DeleteOptions{})
+				Expect(err).ToNot(HaveOccurred())
+			}
+		})
+	})
 })
+
+func nsIncludedInAcrq(acrq *v1alpha1.ApplicationAwareClusterResourceQuota, ns string) bool {
+	for _, acrqns := range acrq.Status.Namespaces {
+		if acrqns.Namespace == ns {
+			return true
+		}
+	}
+	return false
+}
