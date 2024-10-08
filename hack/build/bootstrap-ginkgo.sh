c3447e02450a653fda890ccac93de592782df35f
bmordeha <bmordeha@redhat.com>
2024-10-08 16:26:36 +0300

    Use ginkgo by compling it through vendor To make sure that cli version match the tests versions Use ginkgo directly instead of using the tests binary
    Signed-off-by: bmordeha <bmordeha@redhat.com>

--- hack/build/bootstrap-ginkgo.sh
+++ hack/build/bootstrap-ginkgo.sh
@@ -3,14 +3,13 @@
 set -e
 
 source hack/build/common.sh
+go build -C vendor/github.com/onsi/ginkgo/v2/ginkgo -o /usr/bin/ginkgo
 
 # Find every folder containing tests
 for dir in $(find ${AAQ_DIR}/pkg/ -type f -name '*_test.go' -printf '%h\n' | sort -u); do
     # If there is no file ending with _suite_test.go, bootstrap ginkgo
-    SUITE_FILE=$(find $dir -maxdepth 4 -type f -name '*_test.go')
-    echo ${SUITE_FILE}
-    echo $dir
-    if [ ! -z "$SUITE_FILE" ]; then
-        (cd $dir && ginkgo bootstrap || :)
+    SUITE_FILE=$(find $dir -maxdepth 1 -type f -name '*_suite_test.go')
+    if [ -z "$SUITE_FILE" ]; then
+        (cd $dir && /usr/bin/ginkgo bootstrap || :)
     fi
 done
